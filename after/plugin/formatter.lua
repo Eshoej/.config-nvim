25e31aae41b43493c5809180dee6b82ac2fa9325
Eshoej <morten@eshoej.com>
2025-06-25 12:29:11 +0200

    updated

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -13,6 +13,8 @@ require("conform").setup({
         -- Conform will run the first available formatter
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         html = { "html" },
         yaml = { "yamlfix" },
     },
